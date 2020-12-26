5afcb73137b2379ecd8e988b5268f9b49c496aa7
Michael Paul <michael@michaelpaul.com.br>
2020-12-26 17:07:17 +0000

    Search function and tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,52 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
-    println!(
-        "Searching for \"{}\" in file {}",
-        config.query, config.filename
-    );
-    println!("Contents:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line)
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn no_result() {
+        let res: Vec<&str> = Vec::new();
+        assert_eq!(res, search("foo", "bar"));
+    }
+
+    #[test]
+    fn multiple_results() {
+        let contents = "foo
+nobody
+    somebody
+<body>delete</body>
+bar";
+        assert_eq!(
+            vec!["nobody", "    somebody", "<body>delete</body>"],
+            search("body", contents)
+        );
+    }
 }
