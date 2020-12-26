bd3fe4dc8af11181a0cdfcb5458ef80dff5869ac
Michael Paul <michael@michaelpaul.com.br>
2020-12-26 18:41:54 +0000

    Stderr

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Config problem: {}", err);
+        eprintln!("Config problem: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("App error: {}", e);
+        eprintln!("App error: {}", e);
         process::exit(1);
     }
 }
